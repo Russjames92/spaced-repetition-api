ade632388f70f58ef90cefe0b71a0bb6a6f4a4c0
Russ <18russjames@gmail.com>
2020-11-10 12:00:01 -0800

    edited postgrator

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DATABASE_HOST,
-  "port": process.env.MIGRATION_DATABASE_PORT,
-  "database": process.env.MIGRATION_DATABASE_NAME,
-  "username": process.env.MIGRATION_DATABASE_USER,
-  "password": process.env.MIGRATION_DATABASE_PASS
+  "connectionString": process.env.DATABASE_URL,
 }
