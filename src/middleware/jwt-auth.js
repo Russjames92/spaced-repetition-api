8809917ae064e8fb1f6620bd046d7341619c123a
Russ <18russjames@gmail.com>
2020-11-16 07:45:21 -0800

    final touches

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -5,7 +5,7 @@ async function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
 
   let bearerToken
-  if (!authToken.toLowerCase().startsWith('Bearer ')) {
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
     return res.status(401).json({ error: 'Missing bearer token' })
   } else {
     bearerToken = authToken.slice(7, authToken.length)
